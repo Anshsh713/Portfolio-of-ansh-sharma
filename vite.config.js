3c4afb0822c67a0341f7c65487a5e4a9169e3e24
Ansh Shama <ansh2005sh@gmail.com>
2025-09-16 23:13:31 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-of-ansh-sharma/',
+  base: '/Portfolio-of-ansh-sharma/',  // 👈 IMPORTANT: must match your repo name
 })
+
