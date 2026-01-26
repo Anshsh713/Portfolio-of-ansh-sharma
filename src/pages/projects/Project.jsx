67fe61f72b2c390c0663a33722207a84e9bfe57a
Ansh Shama <ansh2005sh@gmail.com>
2026-01-26 21:57:47 +0530

    profile update

--- src/pages/projects/Project.jsx
+++ src/pages/projects/Project.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import "./Project.css";
 import tic from "./project_photo/tic.png";
-import portfolio from "./project_photo/portfolio.png"
+import portfolio from "./project_photo/portfolio.png";
 function Project() {
   const projects = [
     {
-      img: tic, 
+      img: tic,
       Name: "TIC TAC TOE",
       Github_link: "https://github.com/Anshsh713/TIC_TAC_TOEgame",
-      website_link: "https://anshsh713.github.io/TIC_TAC_TOEgame/home_page.html",
+      website_link:
+        "https://anshsh713.github.io/TIC_TAC_TOEgame/home_page.html",
       about: "Start Playing now and see who will get three in row",
       tags: ["CSS", "JavaScript"],
     },
     {
       img: portfolio,
       Name: "PortFolio",
-      Github_link: "#",
+      Github_link: "https://github.com/Anshsh713/Portfolio-of-ansh-sharma",
       website_link: "#",
       about: "A sleek personal portfolio built with React, Tailwind CSS. ",
       tags: ["React"],
+    },
+    {
+      img: "https://media.istockphoto.com/id/1198430065/vector/attendance-concept-vector-flat-design.jpg?s=612x612&w=0&k=20&c=Q21HKmopEQcN1mhGYNPzRYMWtjla-C-eyGyVEPpoJ5w=",
+      Name: "Attendance",
+      Github_link: "https://github.com/Anshsh713/AtttendenceTracker",
+      website_link: "https://anshsh713.github.io/AtttendenceTracker/#/Home",
+      about: "Track your Attendance with Time Table with React , Appwrite",
+      tags: ["React", "Appwrite"],
     },
   ];
 
@@ -26,51 +35,52 @@ function Project() {
     <div className="projects-container">
       <div className="project-text">
         <h1>Project</h1>
-        <p>Creating is always attractive and I tried to never deprive myself of this pleasure, here you can see the projects published by me.</p>
+        <p>
+          Creating is always attractive and I tried to never deprive myself of
+          this pleasure, here you can see the projects published by me.
+        </p>
       </div>
       <div className="projects-grid">
-              {projects.map((project, index) => (
-        <div key={index} className="project-card">
-          <div className="project-image">
-            {project.img ? (
-              <img src={project.img} alt={project.Name} />
-            ) : (
-              <div className="placeholder">No Image</div>
-            )}
-          </div>
-          <div className="project-details">
-            <h2>{project.Name}</h2>
-            <p>{project.about}</p>
-            <div className="tags">
-              {project.tags.map((tag, i) => (
-                <span key={i} className="tag">
-                  {tag}
-                </span>
-              ))}
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
+            <div className="project-image">
+              {project.img ? (
+                <img src={project.img} alt={project.Name} />
+              ) : (
+                <div className="placeholder">No Image</div>
+              )}
             </div>
-            <div className="link">
-              <a
-                href={project.Github_link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-brands fa-github" title="GitHub"></i>
-              </a>
-              {project.website_link && (
+            <div className="project-details">
+              <h2>{project.Name}</h2>
+              <p>{project.about}</p>
+              <div className="tags">
+                {project.tags.map((tag, i) => (
+                  <span key={i} className="tag">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <div className="link">
                 <a
-                  href={project.website_link}
+                  href={project.Github_link}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button>
-                    View Project
-                  </button>
+                  <i className="fa-brands fa-github" title="GitHub"></i>
                 </a>
-              )}
+                {project.website_link && (
+                  <a
+                    href={project.website_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button>View Project</button>
+                  </a>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
--- src/pages/skills/Skill.jsx
+++ src/pages/skills/Skill.jsx
@@ -1,31 +1,44 @@
 import React from "react";
 import "./Skill.css";
-import js from "./languages/js.png"; 
+import js from "./languages/js.png";
 import python from "./languages/python.png";
 import c from "./languages/letter-c.png";
 import cpp from "./languages/c++.png";
-import html from "./languages/html-5.png";    
-import css from "./languages/css-3.png";  
+import html from "./languages/html-5.png";
+import css from "./languages/css-3.png";
 import react from "./languages/science.png";
 import mysql from "./languages/mysql.png";
 import github from "./languages/github.png";
-import work from "./languages/sorry.png";  
+import work from "./languages/sorry.png";
 import Tailwindcss from "./languages/Tailwind.png";
 const Skill = () => {
   const languages = [
-    {img: js,name: "JavaScript"},
-    {img: c,name: "C"},
-    {img: cpp,name: "C++"}
+    { img: js, name: "JavaScript" },
+    { img: c, name: "C" },
+    { img: cpp, name: "C++" },
   ];
   const Frontend = [
-    {img: html,name: "HTML"},
-    {img: css,name: "CSS"},
-    {img: js,name: "JavaScript"},
-    {img: react,name: "React"},
-    {img: Tailwindcss,name: "Tailwind"}
+    { img: html, name: "HTML" },
+    { img: css, name: "CSS" },
+    { img: js, name: "JavaScript" },
+    { img: react, name: "React" },
+  ];
+  const Backend = [
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpisGYyGorcHGf3rRcISyFKWNMcC8X_51vrg&s",
+      name: "Appwrite",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6bebC_d4eWwJ-x9ntqDuT94TvOgumSBVWHg&s",
+      name: "Node.js",
+    },
   ];
   const Database = [
-    {img: github,name: "GitHub"}
+    { img: github, name: "GitHub" },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSITHn_TgjDyhdWvePNw0mveDrTUr00GLfv_Q&s",
+      name: "MongoDB",
+    },
   ];
   return (
     <div className="skill" id="skills">
@@ -33,13 +46,17 @@ const Skill = () => {
         <h1>Teach Stack</h1>
       </div>
       <div className="skill-content">
-        <p>Here you can see my skills, one of the fascinations of the computer world is the momentary changes of technologies, but I try to update my knowledge every day and experience new technologies.</p>
+        <p>
+          Here you can see my skills, one of the fascinations of the computer
+          world is the momentary changes of technologies, but I try to update my
+          knowledge every day and experience new technologies.
+        </p>
         <div className="skill-box">
           <div className="skill-box1">
             <h2>Languages</h2>
             <div className="languages">
               {languages.map((language, index) => {
-                return(
+                return (
                   <div key={index} className="language-items">
                     <img src={language.img} alt="language" />
                     <p>{language.name}</p>
@@ -51,32 +68,39 @@ const Skill = () => {
           <div className="skill-box2">
             <h2>Frontend Development</h2>
             <div className="languages">
-              {Frontend.map((languages,index)=>{
-                return(
+              {Frontend.map((languages, index) => {
+                return (
                   <div key={index} className="language-items">
                     <img src={languages.img} alt="language" />
                     <p>{languages.name}</p>
                   </div>
-                )
+                );
               })}
             </div>
           </div>
           <div className="skill-box3">
             <h2>Backend Development</h2>
-            <div className="work">
-              <img src={work} alt="work" />
-              </div>
-          </div>
-          <div className="skill-box4">
-            <h2>DataBases</h2>
             <div className="languages">
-              {Database.map((languages,index)=>{
-                return(
+              {Backend.map((languages, index) => {
+                return (
                   <div key={index} className="language-items">
                     <img src={languages.img} alt="language" />
                     <p>{languages.name}</p>
                   </div>
-                )
+                );
+              })}
+            </div>
+          </div>
+          <div className="skill-box4">
+            <h2>DataBases</h2>
+            <div className="languages">
+              {Database.map((languages, index) => {
+                return (
+                  <div key={index} className="language-items">
+                    <img src={languages.img} alt="language" />
+                    <p>{languages.name}</p>
+                  </div>
+                );
               })}
             </div>
           </div>
